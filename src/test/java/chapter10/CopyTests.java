b35945e711f877cf72caf82e1fadfb50f77c1207
dobrodelete <kirkon3004@gmail.com>
2023-07-20 12:46:37 +0300

    add unit tests for task 2

--- src/test/java/chapter10/CopyTests.java
+++ src/test/java/chapter10/CopyTests.java
@@ -3,15 +3,17 @@ package chapter10;
 import book.chapter10.StreamInputOutputUtil;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.nio.file.Paths;
 
 public class CopyTests {
     @Nested
     class CopyDirectory {
-        @ParameterizedTest(name = "Tested {index} task {displayName")
-        @ValueSource(strings = {"srs,target"})
+        @ParameterizedTest(name = "Tested {index} tasks {displayName}")
+        @CsvSource(value = {
+                "src,temp"
+        })
         public void copyDirectory(String source, String target) {
             StreamInputOutputUtil.copyDirectory(Paths.get(source), Paths.get(target));
         }
